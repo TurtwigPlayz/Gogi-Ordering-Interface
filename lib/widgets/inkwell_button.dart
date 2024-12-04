002a5974c5c702853b6a93fb16e5650b4883b9bf
connellr023 <connell.reffo@ucalgary.ca>
2024-12-04 10:31:23 -0700

    Added greyed out button

--- lib/widgets/inkwell_button.dart
+++ lib/widgets/inkwell_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:gogi_ordering_interface/widgets/modals/message_modal.dart';
 
 class InkwellButton extends StatelessWidget {
   const InkwellButton({
@@ -11,6 +12,8 @@ class InkwellButton extends StatelessWidget {
     this.iconSize = 20.0,
     this.borderRadius = 10.0,
     this.splashColor,
+    this.isDisabled = false,
+    this.disabledMessage = 'This button is disabled',
   });
 
   final VoidCallback onTap;
@@ -20,6 +23,22 @@ class InkwellButton extends StatelessWidget {
   final double padding;
   final double iconSize;
   final double borderRadius;
+  final bool isDisabled;
+  final String disabledMessage;
+
+  void _handleTap(BuildContext context) {
+    if (isDisabled) {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) => MessageModal(
+          message: disabledMessage,
+        ),
+      );
+    } else {
+      onTap();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,12 +48,17 @@ class InkwellButton extends StatelessWidget {
     final borderRadius = this.borderRadius *
         clampDouble(MediaQuery.of(context).size.width * 0.001, 1.0, 1.3);
 
+    const double disabledOpacity = 0.65;
+
     return Material(
-      color: Theme.of(context).shadowColor,
+      color: isDisabled
+          ? Theme.of(context).shadowColor.withOpacity(disabledOpacity)
+          : Theme.of(context).shadowColor,
       borderRadius: BorderRadius.circular(borderRadius),
       child: InkWell(
-        splashColor: color.withOpacity(0.75),
-        onTap: onTap,
+        splashColor:
+            isDisabled ? color.withOpacity(0.1) : color.withOpacity(0.75),
+        onTap: () => _handleTap(context),
         borderRadius: BorderRadius.circular(borderRadius),
         child: Padding(
           padding: EdgeInsets.all(padding),
@@ -50,14 +74,17 @@ class InkwellButton extends StatelessWidget {
                         0.7,
                         1.5,
                       ),
-                  color: color,
+                  color:
+                      isDisabled ? color.withOpacity(disabledOpacity) : color,
                 ),
               if (title != null) ...<Widget>[
                 const SizedBox(width: 10.0),
                 Text(
                   title!,
                   style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                        color: color,
+                        color: isDisabled
+                            ? color.withOpacity(disabledOpacity)
+                            : color,
                         fontSize: clampDouble(
                           MediaQuery.of(context).size.width * 0.015,
                           12.0,
